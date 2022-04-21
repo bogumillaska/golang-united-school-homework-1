020f21d8599574ac4ce54f924cf9d0efdb8d732c
Bogumił Laska <bogumil_laska@epam.com>
2022-04-21 13:59:47 +0200

    Change go version

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module solution.go
 
-go 1.13
+go 1.17
 
 require github.com/kyokomi/emoji v2.2.4+incompatible
--- solution.go
+++ solution.go
@@ -1,13 +1,8 @@
-package main
+package solution
 
 import (
-	"fmt"
 	"github.com/kyokomi/emoji"
 )
-
-func main() {
-	fmt.Println(GetMessage())
-}
 
 func GetMessage() string {
 	return emoji.Sprint("Hello :world_map:!")
